4472c92b7a8ed871c6fba7480a7e18d6bb1401a4
Dvvid <jose6dg@gmail.com>
2024-09-23 14:16:05 -0500

    creacion del modelo usuario, en base al ejemplo en clase virtual

--- canineHub/canineHub/urls.py
+++ canineHub/canineHub/urls.py
@@ -20,5 +20,7 @@ from modelUser import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
+     path('admin/usuarios/crear', views.CrearUsuario, name='crear_usuario'),
 ]
--- canineHub/modelUser/views.py
+++ canineHub/modelUser/views.py
@@ -3,6 +3,37 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'home.html')
+
 def register(request):
     form = UserCreationForm()
+    if request.method == 'POST':
+        email = request.POST['email']
+        name = request.POST['name']
+        password = request.POST['password']
+        phone = request.POST['phone']
+        is_boss = request.POST['is_boss']
+        user = User(email=email, name=name, password=password, phone=phone, is_boss=is_boss)
+        if email.is_valid():
+            user.save()
+            return redirect('listar_usuarios')
+        else:
+            return render(request, 'crear_usuario.html')
+    
     return render(request, 'register.html', {'form': form})
+
+
+def CrearUsuario(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        name = request.POST['name']
+        password = request.POST['password']
+        phone = request.POST['phone']
+        is_boss = request.POST['is_boss']
+        user = User(email=email, name=name, password=password, phone=phone, is_boss=is_boss)
+        if email.is_valid():
+            user.save()
+            return redirect('listar_usuarios')
+        else:
+            return render(request, 'crear_usuario.html')
